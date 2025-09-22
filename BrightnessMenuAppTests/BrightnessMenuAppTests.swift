01f7010494b663a224c112f27028c0602523d779
haon2409 <haon2409@gmail.com>
2025-09-22 18:45:52 +0700

    Initial Commit

---
+++ BrightnessMenuAppTests/BrightnessMenuAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrightnessMenuAppTests.swift
+//  BrightnessMenuAppTests
+//
+//  Created by Hao Nguyen on 22/9/25.
+//
+
+import Testing
+@testable import BrightnessMenuApp
+
+struct BrightnessMenuAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
